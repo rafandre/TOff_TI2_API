6af8981a490167ed9ae2ad4567971b65ec7b88bd
Rafael Goncalves <aluno19443@ipt.pt>
2018-07-15 03:30:34 +0100

    Continuação da Estilização da API

--- TOff_TI2_API/Controllers/RealizadorsController.cs
+++ TOff_TI2_API/Controllers/RealizadorsController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Web.Http;
 using System.Web.Http.Description;
 using TimeOff.Models;
@@ -36,8 +38,34 @@ namespace TimeOff.Api
             return Ok(resultado);
         }
 
+        // GET: api/Realizadors/5
+        [HttpGet, Route("api/realizador/{id}")]
+        public IHttpActionResult GetRealizador(int id)
+        {
+            //codigo 
+            var resultado = db.Realizador.Select(
+               aa => new
+               {
+                   id = aa.Id,  // Retorna o Id do Realizador
+                   nome = aa.NomeRealizador, // Retorna o Titulo do Filme 
+                   ano = aa.DataNasc, // Retorna o Ano de Lançamento 
+                   sinopse = aa.Biografia, //Rertorna a Biografia do Realizador 
+                   imagem= aa.ImagemRealizador
+               }).Where(r => r.id == id).First();
+
+            if (resultado == null) return NotFound();
+            var realizador = new {
+                id = resultado.id,  // Retorna o Id do Realizador
+                nome = resultado.nome, // Retorna o Titulo do Filme 
+                ano = resultado.ano.ToString("dd/MM/yyyy"), // Retorna o Ano de Lançamento 
+                sinopse = resultado.sinopse, //Rertorna a Biografia do Realizador 
+                imagem = resultado.imagem
+            };
+            return Ok(realizador);
+        }
+
         // PUT: api/Realizadors/5
-        [ResponseType(typeof(void))]
+        [HttpGet, Route("api/realizador")]
         public IHttpActionResult PutRealizador(int id, Realizador realizador)
         {
             if (!ModelState.IsValid)
